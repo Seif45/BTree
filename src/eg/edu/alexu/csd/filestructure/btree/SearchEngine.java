d52f2a09f21e652b824ef0c3bcc84fae1d1423dd
AbdelRahman AbdelFattah <a.adel.a.fattah@gmail.com>
2020-04-30 08:11:00 +0200

    SearchEngine check if empty

--- src/eg/edu/alexu/csd/filestructure/btree/SearchEngine.java
+++ src/eg/edu/alexu/csd/filestructure/btree/SearchEngine.java
@@ -1,5 +1,7 @@
 package eg.edu.alexu.csd.filestructure.btree;
 
+import javax.management.RuntimeErrorException;
+import java.io.File;
 import java.util.List;
 
 public class SearchEngine implements ISearchEngine {
@@ -7,19 +9,40 @@ public class SearchEngine implements ISearchEngine {
     public SearchEngine(){
 
     }
-
     @Override
     public void indexWebPage(String filePath) {
+        if(isNullOrEmpty(filePath)){
+            throw new RuntimeErrorException(new Error());
+        }
+        File file =  new File(filePath);
+        if(!file.exists()){
+            throw new RuntimeErrorException(new Error());
+        }
 
     }
 
     @Override
     public void indexDirectory(String directoryPath) {
+        if(isNullOrEmpty(directoryPath)){
+            throw new RuntimeErrorException(new Error());
+        }
+        File directory =  new File(directoryPath);
+        if (!directory.exists()){
+            throw new RuntimeErrorException(new Error());
+        }
+
 
     }
 
     @Override
     public void deleteWebPage(String filePath) {
+        if(isNullOrEmpty(filePath)){
+            throw new RuntimeErrorException(new Error());
+        }
+        File file =  new File(filePath);
+        if(!file.exists()){
+            throw new RuntimeErrorException(new Error());
+        }
 
     }
 
@@ -31,5 +54,11 @@ public class SearchEngine implements ISearchEngine {
     @Override
     public List<ISearchResult> searchByMultipleWordWithRanking(String sentence) {
         return null;
+    }
+    private boolean isNullOrEmpty(String str) {
+        if(str != null && !str.isEmpty() && !str.trim().isEmpty()){
+            return false;
+        }
+        return true;
     }
 }
